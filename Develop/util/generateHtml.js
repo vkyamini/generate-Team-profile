700e8417faffa5c64d5e90a0ee53527029ae484f
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-04-23 17:19:13 -0700

    updated html file

--- Develop/util/generateHtml.js
+++ Develop/util/generateHtml.js
@@ -91,8 +91,7 @@ module.exports = team => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
@@ -101,17 +100,15 @@ module.exports = team => {
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+                <h1 class="text-center" card-header >My Team</h1>
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
+   
             <div class="team-area col-12 d-flex justify-content-center">
                 ${generateTeam(team)}
             </div>
-        </div>
-    </div>
+       
 </body>
 </html>
     `;
